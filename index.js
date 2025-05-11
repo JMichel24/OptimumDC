ee9f186ac9f470c4bd1f1e61fb08f08837f00e0e
jorge michel garcia <jmg24.work@gmail.com>
2025-05-10 21:38:46 -0600

    seven commit

--- index.js
+++ index.js
@@ -133,13 +133,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.querySelector('.nav-links');
-
-  if (hamburger && navLinks) {
-    hamburger.addEventListener('click', () => {
-      navLinks.classList.toggle('active');
-    });
-  }
-});
